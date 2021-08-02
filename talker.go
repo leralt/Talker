61c9c7207a5138b8754a714f5e09fde2e0fde605
leralt <htliucl@foxmail.com>
2021-08-02 22:06:47 +0800

    version 0.0.2

--- talker.go
+++ talker.go
@@ -16,11 +16,11 @@ type Person struct {
 //map存储用户
 var onlinePerson map[string]Person
 
-//全局channal传递消息
+//全局channel传递消息
 var massage  = make(chan string)
 
 func MakeMsg(person Person,msg string)(buf string){
-	buf = "["+person.Name+"]: "+msg
+	buf = "> ["+person.Name+"]: "+msg
 	return
 }
 
@@ -33,38 +33,72 @@ func SendMsgToPerson(person Person, conn net.Conn){
 	}
 }
 
-//执行命令
-func ExecCommond(cmd string,conn net.Conn,person *Person){
+// UserName 修改name
+func UserName(conn net.Conn,person *Person)  {
+	BufName :=make([]byte,32)
+	m ,err := conn.Read(BufName)
+	if m == 0 {
+		_, err := conn.Write([]byte("未修改成功...\n"))
+		if err != nil {
+			return
+		}
+	}
+	if err != nil {
+		fmt.Println("read Name error",err)
+	}
+	name := string(BufName[:m])
+	name = strings.Replace(name,"\n","",-1)
+	name = strings.Replace(name,"\r","",-1)
+	name = strings.Trim(name," ")
+	person.Name = name      //不能只修改当前用户的名字
+	onlinePerson[person.Addr] = *person    //要把用户表一并修改
+}
+
+// ExecCommend 执行命令
+func ExecCommend(cmd string,conn net.Conn,person *Person){
 	switch cmd {
 	case "ls":
-		conn.Write([]byte("**************当前用户列表**************\n"))
-		for _, ps := range onlinePerson {
-			conn.Write([]byte(ps.Addr+" : "+ps.Name+"\n"))
-		}
-		conn.Write([]byte("****************************************\n"))
-	case "rename":
-		buf_name :=make([]byte,32)
-		conn.Write([]byte("请输入您的新名字(不超过32个字)："))
-		m ,err := conn.Read(buf_name)
-		if m == 0 {
-			conn.Write([]byte("未修改成功...\n"))
-		}
+		_, err := conn.Write([]byte("**************当前用户列表**************\n"))
 		if err != nil {
-			fmt.Println("read Name error",err)
+			return
 		}
-		name := string(buf_name[:m])
-		name = strings.Replace(name,"\n","",-1)
-		name = strings.Replace(name,"\r","",-1)
-		person.Name = name      //不能只修改当前用户的名字
-		onlinePerson[person.Addr] = *person    //要把用户表一并修改
-		conn.Write([]byte("修改成功~~\n"))
+		for _, ps := range onlinePerson {
+			_, err := conn.Write([]byte(ps.Addr + " : " + ps.Name + "\n"))
+			if err != nil {
+				return
+			}
+		}
+		_, err = conn.Write([]byte("****************************************\n"))
+		if err != nil {
+			return
+		}
+	case "rename":
+		_, err := conn.Write([]byte("请输入您的新名字(不超过32个字)："))
+		if err != nil {
+			return
+		}
+		//修改名字
+		UserName(conn,person)
+		_, err = conn.Write([]byte("修改成功~~\n"))
+		if err != nil {
+			return
+		}
 	case "exit":
-		conn.Write([]byte("您已退出，欢迎下次光临！\n"))
-		conn.Close()
+		_, err := conn.Write([]byte("您已退出，欢迎下次光临！\n"))
+		if err != nil {
+			return
+		}
+		err = conn.Close()
+		if err != nil {
+			return
+		}
 		delete(onlinePerson, person.Addr)
 	default:
 		//conn.Write(buf[1:n])
-		conn.Write([]byte("[Error]没有此命令\n"))
+		_, err := conn.Write([]byte("[Error]没有此命令\n"))
+		if err != nil {
+			return
+		}
 	}
 }
 
@@ -75,7 +109,13 @@ func cmdTrim(cmd string) string{
 	return cmd
 }
 func HandlerConnect(conn net.Conn) {
-	defer conn.Close()
+	defer func(conn net.Conn) {
+		err := conn.Close()
+		if err != nil {
+			fmt.Println("conn Close error",err)
+			return
+		}
+	}(conn)
 	//获取用户IP
 	ipAddr := conn.RemoteAddr().String()
 	//创建用户信息   默认用户名为IP+Port
@@ -90,13 +130,23 @@ func HandlerConnect(conn net.Conn) {
 	//用来给当前用户发送消息
 	go SendMsgToPerson(person,conn)
 	//发送用户上线消息
-	massage <- MakeMsg(person,"*I'm coming!*\n")
+	_, err := conn.Write([]byte("请输入您的名字:"))
+	if err != nil {
+		return
+	}
+	UserName(conn,&person)
+	massage <- MakeMsg(person,"进入房间～\n")
 	//获取用户消息并群发
 	go func() {
 		buf := make([]byte,4096)
 		for {
+			//_, err := conn.Write([]byte("> "))
+			//if err != nil {
+			//	return
+			//}
 			n,err := conn.Read(buf)
 			if n == 0{
+				//删除在线信息
 				delete(onlinePerson, person.Addr)
 				fmt.Println(ipAddr,"用户退出")
 				return
@@ -105,11 +155,13 @@ func HandlerConnect(conn net.Conn) {
 				fmt.Println("conn.Read error",err)
 				return
 			}
+			//用户命令
 			if buf[0] == '#'{
 				cmd := string(buf[1:n])
 				cmd = cmdTrim(cmd)
-				ExecCommond(cmd,conn,&person)
+				ExecCommend(cmd,conn,&person)
 			}else{
+				//用户消息
 				massage <- MakeMsg(person,string(buf[:n]))
 			}
 
@@ -138,7 +190,13 @@ func main(){
 		fmt.Println("Listen error",err)
 		return
 	}
-	defer listener.Close()
+	defer func(listener net.Listener) {
+		err := listener.Close()
+		if err != nil {
+			fmt.Println("listener Close error",err)
+			return
+		}
+	}(listener)
 
 	go Manager()
 
